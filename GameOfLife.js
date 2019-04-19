191a9a7c72db9d868f5ff1a5eb3076bfe8885acd
Adam Sisk <adamsisk91@gmail.com>
2019-04-19 11:33:58 -0500

    makeBoard function

--- GameOfLife.js
+++ GameOfLife.js
@@ -10,15 +10,15 @@ class GameOfLife {
    */
 
   makeBoard() {
-    // TODO: Create and return an 2D Array 
-    // with `this.heigh` as rows and `this.width` as cols.
-    // For example, given a height of 4 and a width of 3, it will generate:
-    // [
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    // ]
+    let newBoard = [];
+    for (let i = 0; i < this.height; ++i) {
+      let tempArr = [];
+      for (let j = 0; j < this.width; ++j) {
+        tempArr.push(0);
+      }
+      newBoard.push(tempArr)
+    }
+    return newBoard;
   }
 
 
